f85b50e641f2f905e32969bfdf8ca46addaa1c0c
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-03-10 14:37:41 +0700

    dfghj

--- app/src/main/java/com/gzeinnumer/uploadservice_wsk4/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/uploadservice_wsk4/MainActivity.java
@@ -2,16 +2,28 @@ package com.gzeinnumer.uploadservice_wsk4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
+    Button btnSelectImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //hasil perubahan
+
+        btnSelectImage =findViewById(R.id.btn_select_image);
+        btnSelectImage.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
